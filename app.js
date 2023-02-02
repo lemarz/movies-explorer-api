35fffb149f33f482af3991395e7e3fd693be5326
Leonid Marz <l3m4rz@gmail.com>
2023-02-02 18:15:44 +0300

    feat: добавить логирование запросов и ошибок

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { errorHandler } = require('./middlewares/errorHandler');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://0.0.0.0:27017/bitfilmsdb');
+app.use(requestLogger);
 
 app.post('/signup', createUser);
 app.post('/signin', login);
@@ -19,6 +21,7 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/movies', require('./routes/movies'));
 
+app.use(errorLogger);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
